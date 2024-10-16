9a49e01abdcb47d0454fd864f73482dc777cba55
EXSSnribanez <nicolas.ibanez@celuweb.com>
2024-10-16 10:59:14 -0500

    settings for scarping

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+import re
+import os
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+from dotenv import load_dotenv
 
 
-driver = webdriver.Chrome()
+load_dotenv()
+
+
+
+download_dir = "/Users/NicolasCw/Desktop/desarrollo/analisis_algoritmos/assets"  # Cambia a la ruta donde quieres que se guarden las descargas
+
+def wait_for_downloads(download_dir):
+    # Esperar mientras haya archivos con la extensión ".crdownload"
+    while any([filename.endswith(".crdownload") for filename in os.listdir(download_dir)]):
+        print("Esperando a que la descarga termine...")
+        time.sleep(1)  # Espera un segundo antes de volver a comprobar
+        
+
+    print("Descarga completa.")
+
+chrome_options = Options()
+chrome_options.add_experimental_option("prefs", {
+  "download.default_directory": download_dir,  # Establece la carpeta de descargas
+  "download.prompt_for_download": False,  # No preguntar por la ubicación de la descarga
+  "download.directory_upgrade": True,  # Actualizar el directorio de descargas si ya está configurado
+  "safebrowsing.enabled": True  # Habilitar el modo de descarga segura
+})
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
 
 # Abrir la página
 driver.get("https://www-scopus-com.crai.referencistas.com/search/form.uri?display=basic#basic")
 
 # Espera implícita para cargar elementos de la página
-driver.implicitly_wait(30)
+# driver.implicitly_wait(10)
 
 # Buscar y hacer clic en el botón para iniciar sesión con Google
 element = driver.find_element(by=By.ID, value="btn-google")
@@ -20,21 +50,30 @@ element.click()
 
 # Buscar el campo de correo electrónico
 element = driver.find_element(by=By.ID, value="identifierId")
-element.send_keys("nicolasr.ibanezp@uqvirtual.edu.co")
+element.send_keys(os.getenv('EMAIL'))
+
 
 # Hacer clic en el botón "Siguiente"
 element = driver.find_element(by=By.ID, value="identifierNext")
 element.click()
 
-#mostarr alerta ingrese la contraseña 
-# show_alert()
+time.sleep(2)
+element = driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')
+element.send_keys(os.getenv('CONTRASENA'))
+# Hacer clic en el botón "Siguiente"2
+element = driver.find_element(by=By.XPATH, value='//*[@id="passwordNext"]/div/button')
+element.click()
 
 
-time.sleep(5)
+
+
+# time.sleep(20)
+driver.implicitly_wait(20)
+
 # Buscar el campo de búsqueda
 input_element = driver.find_element(by=By.CLASS_NAME, value="styleguide-input-module___SqPU")
 print('encontrado')
-input_element.send_keys("algoritmos")
+input_element.send_keys("computational thinking")
 print('escrito')
 
 
@@ -43,6 +82,92 @@ print('escrito')
 search_button = driver.find_element(by=By.XPATH, value="//button[span[text()='Search']]")
 search_button.click()
 
+driver.implicitly_wait(10)
+element = driver.find_element("xpath", '//input[@aria-label="from"]')
+element.send_keys('2020')
+
+element2 = driver.find_element("xpath", '//input[@aria-label="to"]')
+element2.send_keys('')
+element2.send_keys('2024')
+
+time.sleep(1)
+search_button = driver.find_element(by=By.XPATH, value="//button[span[text()='Search']]")
+search_button.click()
+time.sleep(1)
+
+apply_button = driver.find_element(By.XPATH, "//button[@data-testid='apply-facet-range']")
+apply_button.click()
+
+
+
+
+
+documents =  driver.find_element(by= By.CLASS_NAME , value='SearchResultsHeader-module__Qq2ZF' )  
+print ('numero de documentos:' + documents.text)
+data = re.findall(r'\d+', documents.text.replace(',',''))
+docs = int(data[0])
+print('numero de documentos:  '+ str(docs))
+
+# if docs >  2000:
+#     i = docs/2000
+#     i = int(i)
+#     initial = 1
+#     end = 1000
+#     for x in range(i):
+#         print('iteracion: ' + str(x))
+#         if x >= 1:
+#             progress_bar = driver.find_element(By.CSS_SELECTOR, "progress[aria-label='60% completed']")
+#             percent = int(progress_bar.get_attribute("value"))
+#             while percent < 100:
+#                 time.sleep(1)
+#                 progress_bar = driver.find_element(By.CSS_SELECTOR, "progress[aria-label='60% completed']")
+#                 percent = int(progress_bar.get_attribute("value"))
+                
+#             wait_for_downloads(download_dir)
+#             close_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='close']")
+#             close_button.click()
+
+            
+            
+            
+#         dropdown = driver.find_element(by=By.XPATH, value="//button[span[text()='Export']]")
+#         print('encontrado')
+#         dropdown.click()
+#         print('click')
+
+#         time.sleep(5)
+
+#         csv = driver.find_element(by=By.XPATH, value="//button[span[text()='CSV']]")
+#         print('encontrado')
+#         csv.click()
+#         print('click')
+    
+#         radioButtom = driver.find_element(by=By.ID, value="select-range")
+#         radioButtom.click()
+    
+#         fromData = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="From"]')
+#         fromData.clear()
+    
+#         fromData.send_keys(initial)
+    
+    
+#         toData = driver.find_element(By.CSS_SELECTOR, value="input[placeholder=To]")
+#         toData.clear()
+#         toData.send_keys(str(end))
+    
+#         time.sleep(2)
+#         submit_button = driver.find_element(By.CSS_SELECTOR, "button[data-testid='submit-export-button']")
+#         if submit_button.is_enabled():
+                        
+#             submit_button.click()
+
+#         initial = initial + 1000
+#         end = end + 1000
+    
+#         time.sleep(10)
+    
+    
+# else: 
 time.sleep(5)
 
 dropdown = driver.find_element(by=By.XPATH, value="//button[span[text()='Export']]")
@@ -56,20 +181,51 @@ csv = driver.find_element(by=By.XPATH, value="//button[span[text()='CSV']]")
 print('encontrado')
 csv.click()
 print('click')
-
+    
+radioButtom = driver.find_element(by=By.ID, value="select-range")
+radioButtom.click()
+    
+fromData = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="From"]')
+fromData.send_keys('1')
+    
+    
+toData = driver.find_element(By.CSS_SELECTOR, value="input[placeholder=To]")
+toData.send_keys(str(docs))
+    
+time.sleep(2)
 submit_button = driver.find_element(By.CSS_SELECTOR, "button[data-testid='submit-export-button']")
 if submit_button.is_enabled():
     submit_button.click()
 
 
-time.sleep(10)
+time.sleep(5)
 
+#container > micro-ui > document-search-results-page > div.Toastify > div
+progress_bar = driver.find_element(By.CSS_SELECTOR, )
+percent = int(progress_bar.get_attribute("value"))
+while percent < 100:
+    time.sleep(1)
+    progress_bar = driver.find_element(By.XPATH, "//div[@data-testid='file-type-success-message']")
+    try:
+        progress_bar.get_attribute("value")
+        print('valor: ' + progress_bar.get_attribute("value"))
+    except:
+        percent = 100
+        break
+    
+    
+        
 
+time.sleep(5)
+     
+    
 
 
 
 # Espera implícita para el resto de la página
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
+
+
 
 
 
